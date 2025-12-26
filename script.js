d00217dd3ae6760233941be458a74abc4b4a5921
AshutoshPal9161 <palashutosh9161@gmail.com>
2025-12-27 00:38:45 +0530

    modified the js code add some more logic

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-// const alertResult = alert("Are you sure?")
+const alertResult = alert("Are you sure?")
 const isConfirmed = confirm("Would you like to proceed?")
-// const userInput = prompt("Please enter your name.........................")
+const userInput = prompt("Please enter your name.........................")
 console.log(isConfirmed)
+console.log(userInput)
+console.log(alertResult)
+console.log("Script executed successfully.")
