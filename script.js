058e0ca93ed3594c8c456b6fe51b61c358906c58
AshutoshPal9161 <palashutosh9161@gmail.com>
2025-12-29 16:54:48 +0530

    modified the js code

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 
-const alertResult = alert("Are you sure?")
-const isConfirmed = confirm("Would you like to proceed?")
+
+const a = 22
+
+
+// const alertResult = alert("Are you sure?")
+// const isConfirmed = confirm("Would you like to proceed?")
+debugger
 const userInput = prompt("Please enter your name.........................")
-console.log(isConfirmed)
+
+
+
+
+// console.log(isConfirmed)
 console.log(userInput)
-console.log(alertResult)
+// console.log(alertResult)
 console.log("Script executed successfully.")
